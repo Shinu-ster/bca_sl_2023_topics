9f979c50c910b6cf883a09fbe1abf17b634fc961
shinu-ster <sajatbazz@gmail.com>
2024-02-01 23:03:16 +0545

    Improved Code

--- exercise/js/script.js
+++ exercise/js/script.js
@@ -7,25 +7,17 @@
 // 6. make two button
 // first button "random Color" secondbutton "blue"
 
-const change = document.getElementById("changeColor")
-const randColor = document.getElementById("randColor")
-const box1 = document.getElementById("box1")
-change.addEventListener("click",()=>{
-    box1.style.backgroundColor= "blue";
-})
+const change = document.getElementById("changeColor");
+const randColor = document.getElementById("randColor");
+const box1 = document.getElementById("box1");
+change.addEventListener("click", () => {
+  box1.style.backgroundColor = "blue";
+});
 
-
-randColor.addEventListener("click",()=>{
-    let R = Math.random() * 255;
-    let B = Math.random() * 255;
-    let G = Math.random() * 255;
-    R = parseInt(R) 
-    G = parseInt(G) 
-    B = parseInt(B) 
-    R = "20"
-    G = "175"
-    B = "250"
-
-    console.log("rgb("+R+","+G+","+B+")")
-    box1.style.backgroundColor = "rgb("+R+","+G+","+B+")"
-})
+randColor.addEventListener("click", () => {
+  let R = parseInt(Math.random() * 255);
+  let B = parseInt(Math.random() * 255);
+  let G = parseInt(Math.random() * 255);
+  console.log("rgb(" + R + "," + G + "," + B + ")");
+  box1.style.backgroundColor = `rgb(${R},${G},${B})`;
+});
