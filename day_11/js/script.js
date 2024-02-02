ad0be1479fb6dff3427853235aa8cc4350aedf97
shinu-ster <sajatbazz@gmail.com>
2024-02-02 20:17:06 +0545

    Formatted text

--- day_11/js/script.js
+++ day_11/js/script.js
@@ -3,51 +3,56 @@ const firstName = document.getElementById("firstName");
 const lastName = document.getElementById("lastName");
 const userName = document.getElementById("username");
 const passkey = document.getElementById("passkey");
-const errorMsg = document.getElementById("errorMsg")
-const showForm = document.getElementById("showForm")
+const errorMsg = document.getElementById("errorMsg");
+const showForm = document.getElementById("showForm");
 // defining btn
 const btnReg = document.getElementById("btnReg");
 
 // defining form
-const regForm = document.getElementById("regForm")
-
+const regForm = document.getElementById("regForm");
 
 // submit event
-document.addEventListener('DOMContentLoaded',function(){
-btnReg.addEventListener('click',function(event){
-    if (firstName.value === "" || lastName.value === "" || userName.value === "" || passkey.value === "") {
-        event.preventDefault()
-        errorMsg.innerHTML = "All fields are required"
+document.addEventListener("DOMContentLoaded", function () {
+  btnReg.addEventListener("click", function (event) {
+    if (
+      firstName.value === "" ||
+      lastName.value === "" ||
+      userName.value === "" ||
+      passkey.value === ""
+    ) {
+      event.preventDefault();
+      errorMsg.innerHTML = "All fields are required";
     }
-
-});
+  });
 });
 
 // user submit event on form
-regForm.addEventListener("submit",function(event){
-    console.log("Form is submitted")
-})
+regForm.addEventListener("submit", function (event) {
+  console.log("Form is submitted");
+});
 
 // arrow function eample
 // shorter form of function
 
 // ordinary funciton
-function showMessage(){ return "Hello! How are you?" }
+function showMessage() {
+  return "Hello! How are you?";
+}
 
 // arrow function
-const msg= ()=>{ return "Hello! How are you?" }
+const msg = () => {
+  return "Hello! How are you?";
+};
 
 // if single return function then
-const message = () =>{
-    "Hello! How are you"
-}
+const message = () => {
+  "Hello! How are you";
+};
 
 // displaying form with event and hidden property
 // of element
 
 // to toggle form
-showForm.addEventListener("click",()=>{
-    regForm.hidden = !regForm.hidden
-})
-
-
+showForm.addEventListener("click", () => {
+  regForm.hidden = !regForm.hidden;
+});
